120c29044f03e70c25c2ce81f3a37af490afa976
namarrehman <namarrehman@gmail.com>
2018-02-07 17:03:52 +0300

    Update rectangle_overlap.py

--- rectangle_overlap.py
+++ rectangle_overlap.py
@@ -11,3 +11,41 @@ Yes, up is down! This is quite common in computer graphics, and is how Tkinter a
 Check out the examples in the test code we provided to see this in action.
 Up is down. Weird, but true
 '''
+def rectanglesOverlap(x1, y1, w1, h1, x2, y2, w2, h2):
+    #below said equuation are used for finding smallest cordinates
+    X1=min(x1,x2)
+    Y1=min(y1,y2)
+    W1=min(w1,w2)
+    H1=min(h1,h2)
+    X2=max(x1,x2)
+    Y2=max(y1,y2)
+    W2=max(w1,w2)
+    H2=min(h1,h2)
+    
+    print("X1",X1)
+    print("Y1",Y1)
+    print("W1",W1)
+    print("H1",H1)
+    print("X2",X2)
+    print("Y2",Y2)
+    print("W2",W2)
+    print("H2",H2)
+    print(int(X1)+int(W1))
+    print(int(X1+W1))
+    print(int(Y1+H1))
+    if(int(X1)+int(W1)>=int(X2) and int(Y1)+int(H1)>=int(Y2)):
+        print("both the graphs will oevrlap")
+    else:
+        print("both the graphs  will not overlap")
+        
+ 
+x1=input("enter the value for x1")
+y1=input("enter the value for y1")
+w1=input("enter the value for w1")
+h1=input("enter the value for h1")
+x2=input("enter the value for x2")
+y2=input("enter the value for y2")
+w2=input("enter the value for w2")
+h2=input("enter the value for h2")   
+
+rectanglesOverlap(x1, y1, w1, h1, x2, y2, w2, h2)
