e19cf7885656d732f3059ce4606678e57ff16c29
namarrehman <namarrehman@gmail.com>
2018-03-04 20:09:24 +0300

    isRightTriangle

--- isRightTriangle.py
+++ isRightTriangle.py
@@ -1,8 +1,10 @@
 ''''
 isRightTriangle(x1, y1, x2, y2, x3, y3) [25 pts]
- Write the function isRightTriangle(x1, y1, x2, y2, x3, y3) that takes 6 int or float values that represent the vertices (x1,y1), (x2,y2), and (x3,y3) of a triangle, and returns True if that is a right triangle and False otherwise. You may wish to write a helper function, distance(x1, y1, x2, y2), which you might call several times. Also, remember to use almostEqual (instead of ==) when comparing floats.
+Write the function isRightTriangle(x1, y1, x2, y2, x3, y3) that takes 
+6 int or float values that represent the vertices (x1,y1), (x2,y2), and (x3,y3) of a triangle, and returns True if that is a right triangle and False otherwise.
+You may wish to write a helper function, distance(x1, y1, x2, y2), which you might call several times. Also, remember to use almostEqual (instead of ==) when comparing floats.
 '''
-
+import math
 
 def isRightTriangle(x1,y1,x2,y2,x3,y3):
     #Varaibles pq,pr,qr are used to find length of each sides
@@ -12,6 +14,9 @@ def isRightTriangle(x1,y1,x2,y2,x3,y3):
     print(pr)
     qr=math.sqrt(math.pow((x2-x3),2)+math.pow((y2-y3),2))
     print(qr)
+    c=0
+    a=0
+    b=0
     if(pq<=pr<=qr):#Used for finding the largest side
         c=qr
         a=pr
@@ -24,8 +29,13 @@ def isRightTriangle(x1,y1,x2,y2,x3,y3):
         c=pq
         a=qr
         b=pr
-    if( math.pow(c,2)==(math.pow(a,2) + math.pow(b,2))):
+    if( round(math.pow(c,2))==round((math.pow(a,2)) + round(math.pow(b,2)))):
         print("right traingle")
+    else:
+        print("not right triangle")
         
 
-isRightTriangle(-7,4,6,-2,0,-15)
+
+#isRightTriangle(-7,4,6,-2,0,15)
+
+isRightTriangle(-2,-3,2,1,5,-2)
