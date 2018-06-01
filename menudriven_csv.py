2b096a888b15c6a2a3cedbcad070b746cec723c7
namarrehman <namarrehman@gmail.com>
2018-06-02 00:42:09 +0300

    Add files via upload

--- menudriven_csv.py
+++ menudriven_csv.py
@@ -1,30 +1,19 @@
-print('update-->3')
-print('delete-->2')
-name=[]
-clas=[]
-mark=[]
-str2=[]
-i=0
-
 def update(str1):
         str3=''
         rownum=input("enter the row number(from 1 onwards):")
-        field=input("enter the cloumn name:")
-        fvalue=input("enter the filed value:")
-        fo=open("book.csv","r")
-        for line in fo:
-            str2.append(line)
-        x=str2[int(rownum)].split(",")
-        if(field=='name'):
-            column=0
-        if(field=='class'):
-            column=1
-        if(field=='mark'):
-            column=2
-        if (column==len(x)-1):
-           x[column]=fvalue+'\n'
+        columnname=input("enter the cloumn name:")
+        fvalue=input("enter the field value:")
+        x=str1[int(rownum)].split(",")
+        columns=str1[0].split(",")#to identify column header
+        print(columns)
+        for i in range(0,len(columns)):
+            if(columns[i]==columnname)or columns[i]==(columnname+'\n'):#validate column header with enetered value
+                colmnpos=i
+                break
+        if (colmnpos==len(x)-1):
+           x[colmnpos]=fvalue+'\n'
         else:
-           x[column]=fvalue
+           x[colmnpos]=fvalue
         for i in range(0,len(x)):
                 if(i==len(x)-1):
                          str3=str3+x[i]
@@ -39,38 +28,38 @@ def dell(str1):
         return str1
 
 def save(str1):
-        fo=open("book.csv","w+")
-        fo.seek(0,0)
+        fo=open("book.csv","w")
         for i in range(0,len(str1)):
                 fo.write("%s"%str1[i])
-
-        
-j=1
-filecont=[]
+def viewfile():
+        filecont=[]
+        fo=open("book.csv","r")
+        for line in fo:
+                filecont.append(line)
+        print('file contents',filecont)
+        fo.close
 fo=open("book.csv","r")
-for line in fo:
-        filecont.append(line)
-print('file contents',filecont)
-fo.close
-x=input('enter the menu option')
-fo.close
 str1=[]
-str2=[]
-fo=open("book.csv","r+")
 for line in fo:
         str1.append(line)
-fo.close()
-if(int(x)==2):
-        str1=dell(str1)
-        save(str1)
+while(1):
+        print('update-->1')
+        print('delete-->2')
+        print('exit---->3')
 
-if(int(x)==3):
-        str2=update(str1)
-        save(str2)
-fo=open("book.csv","r")
-filecont=[]
-for line in fo:
-        filecont.append(line)
-print('filecontens',filecont)
-fo.close
-x=input("enter any key to exit")
+        x=input('enter the menu option')
+        if(int(x)==3):
+                break
+        else:
+                if(int(x)==2):
+                        str1=dell(str1)
+                        print(str1)
+                        save(str1)
+
+                if(int(x)==1):
+                        str1=update(str1)
+                        print(str1)
+                        save(str1)
+
+
+                
